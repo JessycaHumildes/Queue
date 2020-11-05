a53b60abaced4ef05d43561925e990d6dcb6cd04
JessycaHumildes <jessycahumildes@hotmail.com>
2020-11-05 22:07:53 +0000

    RoundQueue

--- src/com/jhumildes/ArrayQueue.java
+++ src/com/jhumildes/ArrayQueue.java
@@ -13,13 +13,35 @@ public class ArrayQueue {
     }
 
     public void add(Employee employee){
-        if(back ==queue.length){
+
+        if(size() ==queue.length - 1){
+            int numItems = size();
             Employee[] newArray = new Employee[2 * queue.length];
-            System.arraycopy(queue,0, newArray, 0, queue.length);
+
+            System.arraycopy(queue, front, newArray, 0, queue.length - front);
+            System.arraycopy(queue,0, newArray, queue.length - front, back);
+
             queue = newArray;
+
+            front = 0;
+            back = numItems;
         }
+
+        //0
+        //1
+        //2 empl3 front
+        //3 empl4
+        //4 empl5
+        //5       back
+
+
+
         queue[back] = employee;
-        back++;
+        if (back < queue.length - 1){
+            back++;
+        }else{
+            back = 0;
+        }
     }
     public Employee remove(){
         //check if the queue is empty
@@ -32,6 +54,9 @@ public class ArrayQueue {
         if (size() == 0){
             front = 0;
             back = 0;
+        }
+        else if (front == queue.length){
+            front = 0;
         }
         return employee;
     }
@@ -47,13 +72,28 @@ public class ArrayQueue {
 
     public int size(){
 
-        return back - front;
+        if (front <= back){
+            return back - front;
+        }else {
+            return back - front + queue.length;
+        }
+
     }
 
     public void printQueue(){
+        if (front <= back){
         for (int i = front; i < back; i++){
             System.out.println(queue[i]);
         }
     }
+    else{
+        for (int i = front; i < queue.length; i++){
+            System.out.println(queue[i]);
+        }
+        for (int i = 0; i < back; i++){
+            System.out.println(queue[i]);
+        }
+    }
 
-}
+
+}}
--- src/com/jhumildes/Main.java
+++ src/com/jhumildes/Main.java
@@ -9,15 +9,31 @@ public class Main {
         Employee JakeYoung = new Employee("Jake","Young", 6453);
         Employee BiaLinda = new Employee("Bia", "Linda", 3745);
         Employee DoraLicia = new Employee("Dora","Licia", 6574);
+        Employee WiviBento = new Employee("Wivi", "Bento", 3467);
 
-        ArrayQueue queue = new ArrayQueue(10);
+
+        ArrayQueue queue = new ArrayQueue(5);
+
         queue.add(JessycaHumildes);
         queue.add(JakeYoung);
-        queue.add(BiaLinda);
-        queue.add(DoraLicia);
-        //queue.printQueue();
-
         queue.remove();
+        queue.add(BiaLinda);
+        queue.remove();
+        queue.add(DoraLicia);
+        queue.remove();
+        queue.add(WiviBento);
+        queue.printQueue();
+
+//
+//        queue.add(JessycaHumildes);
+//        queue.add(JakeYoung);
+//        queue.add(BiaLinda);
+//        queue.add(DoraLicia);
+//        queue.add(WiviBento);
+//        queue.printQueue();
+
+        //queue.printQueue();
+        //queue.remove();
         //queue.printQueue();
 
         System.out.println(queue.peek());
